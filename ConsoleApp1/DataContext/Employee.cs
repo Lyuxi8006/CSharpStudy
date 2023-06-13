36f19b2b6c92004e83d77669ba20769996496562
Lyuxi <hhackerbox@163.com>
2023-06-13 22:35:45 +0800

    wpf binding context

---
+++ ConsoleApp1/DataContext/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContext
+{
+    public class Employee
+    {
+        public int Id { get; set; } = 2;
+        public string Name { get; set; } = "Mike";
+    }
+}
