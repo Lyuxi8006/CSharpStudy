c0b0484bda619d98d5148c3cea4bd515c1b00fb2
Lyuxi <hhackerbox@163.com>
2023-06-28 21:45:12 +0800

    Bullet Controls and ToolTip

---
+++ ConsoleApp1/WPF_ToolTip/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ToolTip
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyStackPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DP1_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
