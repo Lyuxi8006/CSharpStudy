53d6b5a6da9a8c78171302953c469313bfc7900f
Lyuxi <hhackerbox@163.com>
2023-07-08 21:49:47 +0800

    Data Grid

---
+++ ConsoleApp1/WPF_CustomDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_CustomDataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Songs
+    {
+        public List<Song> SongList { get; set; } = GetSongs();
+        public static List<Song> GetSongs()
+        {
+            var file = @"D:\Lyuxi\WPF\CSharpStudy\ConsoleApp1\WPF_CustomDataGrid\Assets\Songs.csv";
+            var lines = File.ReadAllLines(file);
+            var list = new List<Song>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i].Split(',');
+                var g = line[2].Split(' ', '&', '-');
+                var gr = g.Length > 1 ? g[0] + g[1] : g[0];
+                var song = new Song()
+                {
+                    Id = int.Parse(line[0]),
+                    Title = line[1],
+                    Artist = line[3],
+                    IsSoundtrack = line[4] == "Unknown" ? false : true,
+                    MovieTitle = line[4],
+                    Genre = (Genre)Enum.Parse(typeof(Genre), gr),
+                    ResealeYear = DateTime.Parse(line[5] + ",1,1"),
+                    URL = new Uri($"www.{line[3]}.com", UriKind.Relative),
+                };
+                list.Add(song);
+            }
+            return list;
+        }
+
+
+    }
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public bool IsSoundtrack { get; set; }
+        public string MovieTitle { get; set; }
+        public Genre Genre { get; set; }
+        public DateTime ResealeYear { get; set; }
+        public Uri URL { get; set; }
+    }
+    public enum Genre
+    {
+        HeavyMetal, HardRock, SoftRock, ClassicRock, Rock, Pop, PopSoul, Soul, Blues, Jazz, RB, Country, Folk, Funk, Classical, ChristmasCarol
+    }
+}
