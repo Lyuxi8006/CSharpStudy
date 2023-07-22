c838c61e996a256836e678c7904293ebcf033319
Lyuxi <hhackerbox@163.com>
2023-07-22 14:14:36 +0800

    draw 2d

---
+++ ConsoleApp1/WPF_GeometryGroup/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WPF_GeometryGroup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Path path = null;
+        private double minLength = 10;
+        private double pointLength = 1;
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void canDraw_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 右键按下转实线
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                if (path != null)
+                {
+                    if (path.Data is GeometryGroup)
+                    {
+                        GeometryGroup geometryGroup = path.Data as GeometryGroup;
+                        if (geometryGroup.Children.Count > 3)
+                        {
+
+                            Polygon new_polygon = new Polygon();
+                            new_polygon.Stroke = Brushes.Blue;
+                            new_polygon.StrokeThickness = 2;
+                            for (int i = 0; i < geometryGroup.Children.Count; ++i)
+                            {
+                                if (geometryGroup.Children[i] is EllipseGeometry ellipse)
+                                {
+                                    new_polygon.Points.Add(ellipse.Center);
+                                }
+
+                            }
+                            canDraw.Children.Add(new_polygon);
+                        }
+                    }
+                    canDraw.Children.Remove(path);
+                    path = null;
+                }
+                return;
+            }
+
+            // 虚线点,用GeometryGroup包含每个点
+            if (path == null)
+            {
+                canDraw.Children.Clear();
+                path = new Path()
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2,
+                    Fill = Brushes.Blue
+                };
+                GeometryGroup geometryGroup = new GeometryGroup();
+                geometryGroup.Children.Add(new EllipseGeometry() { Center = e.GetPosition(canDraw), RadiusX = pointLength, RadiusY = pointLength });
+                path.Data = geometryGroup;
+                canDraw.Children.Add(path);
+            }    
+        }
+
+        private void canDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (path == null) 
+                return;
+            if (path.Data is GeometryGroup)
+            {
+                var geometryGroup = path.Data as GeometryGroup;
+                if (geometryGroup.Children.Count > 0 && geometryGroup.Children[geometryGroup.Children.Count-1]  is EllipseGeometry) 
+                {
+                    EllipseGeometry ellipse = geometryGroup.Children[geometryGroup.Children.Count - 1] as EllipseGeometry;
+                    var point = e.GetPosition(canDraw);
+                    double dist = (ellipse.Center - point).Length;
+                    if (dist > minLength) 
+                    {
+                        double deltaX = (point.X - ellipse.Center.X) / dist;
+                        double deltaY = (point.Y - ellipse.Center.Y) / dist;
+                        for (; dist > minLength;)
+                        {
+                            double destX = ellipse.Center.X + deltaX * minLength;
+                            double destY = ellipse.Center.Y + deltaY * minLength;
+                            Point finalPoint = new Point() { X = destX, Y = destY };
+                            geometryGroup.Children.Add(new EllipseGeometry() { Center = finalPoint, RadiusX = pointLength, RadiusY = pointLength });
+                            dist -= minLength;
+                        }
+                    }
+                    if (geometryGroup.Children.Count > 10)
+                    {
+                        EllipseGeometry firstEllipse = geometryGroup.Children[0] as EllipseGeometry;
+                        if (firstEllipse != null)
+                        {
+                        
+                            double toFirstPointDist = (firstEllipse.Center - point).Length;
+                            if (toFirstPointDist < minLength)
+                            {
+                                if (path != null)
+                                {
+                                    if (path.Data is GeometryGroup)
+                                    {
+                                        Polygon new_polygon = new Polygon();
+                                        new_polygon.Stroke = Brushes.Blue;
+                                        new_polygon.StrokeThickness = 2;
+                                        for (int i = 0; i < geometryGroup.Children.Count; ++i)
+                                        {
+                                            if (geometryGroup.Children[i] is EllipseGeometry t)
+                                            {
+                                                new_polygon.Points.Add(t.Center);
+                                            }
+
+                                        }
+                                        canDraw.Children.Add(new_polygon);
+                                    }
+                                    canDraw.Children.Remove(path);
+                                    path = null;
+                                }
+                                return;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/WPF_MoveShapeWithMouse/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_MoveShapeWithMouse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Point DragStartPoint, DragEndPoint, ObjectStartLocation;
+        object ClickedObject;
+        double ClickedLineLength;
+        private void DrawingCanvas_OnPreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ContextMenu cm = new ContextMenu();
+            cm.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
+            MenuItem RectangleButton = new MenuItem() { Header = "RECTANGLE" };
+            MenuItem CircleButton = new MenuItem() { Header = "CIRCLE" };
+            MenuItem LineButtton = new MenuItem() { Header = "LINE" };
+
+            RectangleButton.Click += RectangleButton_Click;
+            CircleButton.Click += CircleButton_Click;
+            LineButtton.Click += LineButtton_Click;
+
+            cm.Items.Add(RectangleButton);
+            cm.Items.Add(CircleButton);
+            cm.Items.Add(LineButtton);
+            cm.IsOpen = true;
+        }
+
+
+
+        private void RectangleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle ARectangle = new Rectangle() { Height = 150, Width = 150, Stroke = Brushes.Black, StrokeThickness = 5, Fill = Brushes.Red };
+            Canvas.SetLeft(ARectangle, (DrawingCanvas.ActualWidth / 2) - (ARectangle.Width / 2));
+            Canvas.SetTop(ARectangle, (DrawingCanvas.ActualHeight / 2) - (ARectangle.Height / 2));
+            ARectangle.Tag = "R" + (DrawingCanvas.Children.Count).ToString();
+            ARectangle.PreviewMouseLeftButtonDown += ARectangleOnPreviewMouseLeftButtonDown;
+            DrawingCanvas.Children.Add(ARectangle);
+        }
+
+
+        private void ARectangleOnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle r = sender as Rectangle;
+            DragStartPoint.X = e.GetPosition(this).X;
+            DragStartPoint.Y = e.GetPosition(this).Y;
+            ObjectStartLocation.X = Canvas.GetLeft(r);
+            ObjectStartLocation.Y = Canvas.GetTop(r); ;
+            ClickedObject = r; 
+
+        }
+
+        private void CircleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Ellipse ACircle = new Ellipse() { Height = 150, Width = 150, Stroke = Brushes.Black, StrokeThickness = 5, Fill = Brushes.Blue };
+            Canvas.SetLeft(ACircle, (DrawingCanvas.ActualWidth / 2) - (ACircle.Width / 2));
+            Canvas.SetTop(ACircle, (DrawingCanvas.ActualHeight / 2) - (ACircle.Height / 2));
+            ACircle.Tag = "C" + (DrawingCanvas.Children.Count).ToString();
+            ACircle.PreviewMouseLeftButtonDown += ACircleOnPreviewMouseLeftButtonDown;
+            DrawingCanvas.Children.Add(ACircle);
+        }
+
+        private void ACircleOnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse c = sender as Ellipse;
+            DragStartPoint.X = e.GetPosition(this).X;
+            DragStartPoint.Y = e.GetPosition(this).Y;
+            ObjectStartLocation.X = Canvas.GetLeft(c);
+            ObjectStartLocation.Y = Canvas.GetTop(c); ;
+            ClickedObject = c;
+        }
+
+        private void LineButtton_Click(object sender, RoutedEventArgs e)
+        {
+            double LineWidth = 150;
+            Line Aline = new Line() { Stroke = Brushes.Black, StrokeThickness = 5, Fill = Brushes.Red };
+
+            Aline.X1 = (DrawingCanvas.ActualWidth / 2) - (LineWidth / 2);
+            Aline.Y1 = (DrawingCanvas.ActualHeight / 2);
+            Aline.X2 = Aline.X1 + LineWidth;
+            Aline.Y2 = (DrawingCanvas.ActualHeight / 2);
+            Aline.Tag = "L" + (DrawingCanvas.Children.Count).ToString();
+            Aline.PreviewMouseLeftButtonDown += AlineOnPreviewMouseLeftButtonDown;
+            DrawingCanvas.Children.Add(Aline);
+        }
+
+
+        private void AlineOnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Line l = sender as Line;
+            ClickedLineLength = Math.Abs(l.X1 - l.X2);
+            DragStartPoint.X = e.GetPosition(this).X;
+            DragStartPoint.Y = e.GetPosition(this).Y;
+            ObjectStartLocation.X = l.X1;
+            ObjectStartLocation.Y = l.Y1;
+            ClickedObject = l;
+        }
+
+
+        private void DrawingCanvas_OnPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (ClickedObject == null)
+            {
+                return;
+            }
+
+            DragEndPoint.X = e.GetPosition(this).X;
+            DragEndPoint.Y = e.GetPosition(this).Y;
+            double deltaX = DragEndPoint.X - DragStartPoint.X;
+            double deltaY = DragEndPoint.Y - DragStartPoint.Y;
+            if (ClickedObject is Rectangle)
+            {
+                Rectangle r = ClickedObject as Rectangle;
+                Canvas.SetLeft(r, ObjectStartLocation.X + deltaX);
+                Canvas.SetTop(r, ObjectStartLocation.Y + deltaY);
+            }
+            else if (ClickedObject is Ellipse)
+            {
+                Ellipse c = ClickedObject as Ellipse;
+                Canvas.SetLeft(c, ObjectStartLocation.X + deltaX);
+                Canvas.SetTop(c, ObjectStartLocation.Y + deltaY);
+            }
+            else if (ClickedObject is Line)
+            {
+
+                Line l = ClickedObject as Line;
+
+                l.X1 = ObjectStartLocation.X + deltaX;
+                l.Y1 = ObjectStartLocation.Y + deltaY;
+                l.X2 = l.X1 + ClickedLineLength;
+                l.Y2 = ObjectStartLocation.Y + deltaY;
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        private void DrawingCanvas_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ClickedObject = null;
+        }
+    }
+
+}
