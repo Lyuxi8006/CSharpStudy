9da682bf6aee3a5a821e18a97bb0be541d4a97e8
Lyuxi <hhackerbox@163.com>
2023-08-03 21:45:21 +0800

    fix and flow document

---
+++ ConsoleApp1/FixedDocument/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FixedDocument
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CteatePage();
+        }
+        void CteatePage()
+        {
+            ListBox lbx = new ListBox();
+            for (int i = 0; i < 50; i++)
+            {
+                lbx.Items.Add("Book " + i);
+            }
+            PageContent pc = new PageContent();
+            FixedPage fp = new FixedPage();
+            fp.Children.Add(lbx);
+            ((IAddChild)pc).AddChild(fp);
+            FxDoc.Pages.Add(pc);
+        }
+    }
+}
