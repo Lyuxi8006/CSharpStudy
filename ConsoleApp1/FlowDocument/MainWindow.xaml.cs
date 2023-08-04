d5b7722e857bf992ab7d3228eef42e5d83150ea3
Lyuxi <hhackerbox@163.com>
2023-08-04 21:58:54 +0800

    fix and flow document

--- ConsoleApp1/FlowDocument/MainWindow.xaml.cs
+++ ConsoleApp1/FlowDocument/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,54 @@ namespace FlowDocument
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog pd = new PrintDialog();
+            if(pd.ShowDialog() == true ) 
+            {
+                IDocumentPaginatorSource ipd = FlowDoc;
+                pd.PrintDocument(ipd.DocumentPaginator, "Flow Document");
+            }
+        }
+
+
+        private void SaveTXT_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange range;
+            FileStream stream;
+            range = new TextRange(RichTBX.Document.ContentStart, RichTBX.Document.ContentEnd);
+            stream = new FileStream("myRTBcontent.txt", FileMode.Create);
+            range.Save(stream, DataFormats.Text);
+        }
+
+        private void SaveXAML_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("myRTBcontent.xaml"))
+            {
+                TextRange range;
+                FileStream stream;
+                range = new TextRange(RichTBX.Document.ContentStart, RichTBX.Document.ContentEnd);
+                stream = new FileStream("myRTBcontent.xaml", FileMode.Create);
+                range.Save(stream, DataFormats.XamlPackage);
+            }
+        }
+
+        private void SaveRTF_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists("myRTBcontent.rtf"))
+            {
+                TextRange range;
+                FileStream stream;
+                range = new TextRange(RichTBX.Document.ContentStart, RichTBX.Document.ContentEnd);
+                stream = new FileStream("myRTBcontent.rtf", FileMode.Create);
+                range.Save(stream, DataFormats.Rtf);
+            }
+            else
+            {
+                MessageBox.Show("file already exists");
+            }
         }
     }
 }
